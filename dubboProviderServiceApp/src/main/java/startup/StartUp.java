8d096deea7620e81c020199d7dd6a89e4116ea20
unknown <chenfei1@7F-Chenfei1.globale.com>
2016-10-31 10:54:54 +0800

    first commit

---
+++ dubboProviderServiceApp/src/main/java/startup/StartUp.java
@@ -0,0 +1,51 @@
+package startup;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class StartUp {
+	
+	private final static Logger logger = LoggerFactory.getLogger(StartUp.class);
+	
+	public static void main(String[] args) {
+		//启动服务
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:application.xml");
+		context.start();
+		
+		Runtime.getRuntime().addShutdownHook(new Thread(){
+
+			@Override
+			public void run() {
+				if(context != null){
+					context.stop();
+					context.close();
+					logger.info("dubboProviderServiceApp is stop !!!");
+				}
+				
+				synchronized (StartUp.class) {
+					StartUp.class.notify();
+					
+				}
+			}
+			
+		});
+			
+		
+		synchronized (StartUp.class) {
+			try {
+				StartUp.class.wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		logger.info("dubboProviderServiceApp start success !!!");
+		System.out.println("service startup successs ……");
+		
+		//while(true);
+		
+		
+	}
+
+}
